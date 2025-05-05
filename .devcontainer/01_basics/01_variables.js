71c1a4c3d8729f123d2b8d39852072ef522d7550
Koushik Panda <koushik.3202@gmail.com>
2025-05-05 09:05:26 +0000

    Variiables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 113344
+let accountEmail = "hitesh@google.com"
+var accountPasswor  = "12345"
+accountCity = "Kolkata"
+
+//accountId = 2
+
+accountEmail = "hc@cc.com"
+accountPasswor ="21211"
+accountCity = "durgapur"
+let accountstate;
+
+console.log(accountEmail);
+
+/*
+Please  not use var
+Because of issue in block scope and function scope
+*/
+
+console.table([accountId, accountEmail, accountPasswor, accountCity, accountstate])
+
