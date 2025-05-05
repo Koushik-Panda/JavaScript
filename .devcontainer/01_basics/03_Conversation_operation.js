abd798c27bed0441d806fbd06ce3c4bda32c8b48
Koushik Panda <koushik.3202@gmail.com>
2025-05-05 12:04:32 +0000

    Conversation

---
+++ .devcontainer/01_basics/03_Conversation_operation.js
@@ -0,0 +1,32 @@
+let score = undefined 
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN (Not a Number) => But its type a number
+// true => 1
+// false => 0
+// null => 0
+// undefined => Nan
+
+
+let isLoggedIn = "kk"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true; 0 => false 
+//"" => false
+// "hitesh" => true 
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
