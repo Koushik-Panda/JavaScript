9640025e8171bf5f5a67a6b124ad974a9f9f4b45
Koushik Panda <koushik.3202@gmail.com>
2025-05-06 13:27:46 +0000

    Data_Types_Summary

--- .devcontainer/01_basics/05_Data_Types_Summary.js
+++ .devcontainer/01_basics/05_Data_Types_Summary.js
@@ -63,3 +63,11 @@ const myFunction = function(){
 //non premative typeof is fuction and fuction type of objectfunction
 
 console.log(typeof id)
+
+//datatypes are based on memory allocation
+// Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+// Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+// JavaScript is a dynamically typed language. This means that you don't need to specify the data type of a variable when you declare it.
+
