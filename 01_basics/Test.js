a7129d7e4ddeea4620468b83f8e22d1a31ccfe0c
Koushik Panda <koushik.3202@gmail.com>
2025-05-08 09:47:19 +0000

    Test

--- 01_basics/Test.js
+++ 01_basics/Test.js
@@ -1,2 +1,53 @@
-console.log("Hitesh")
-console.log("Koushik")
+// const id = 1226341 //const use for permanently assigned, can't chnage
+// // const id =122234
+
+// console.log(id)
+
+// let iam = 111122222 // let use for temporary assigned, can change
+// iam = 1122333
+
+// console.log(iam)
+
+// "use strict"  if it is enable then in output it shows bad practice
+//               special directive in JavaScript that tells the browser (or Node.js) 
+//               to run your code in Strict Mode — a safer, cleaner version of JavaScript.
+
+// acoountEmail="koushik2334@gmail.com" // it is way of define variable but it's generally considered bad practice.
+// console.log(acoountEmail)
+
+// console.log(3+3)
+
+// console.log("Koushik")
+
+// let name = "Koushik"
+// let age = 29
+// let addrees
+
+// console.table([name, age, addrees])
+
+
+
+// null is use for inteaonally empty. If some dat come after then store in null varible
+// with null value you can store any type of value in null variable.
+//ex:
+
+// let a = null
+// console.log(a)
+
+// a = {name:"clark", age:29}
+// console.log(a)
+
+
+let s
+let t = Number(s)
+let c=5
+
+console.log(typeof s)
+console.log(typeof t)
+console.log(typeof c)
+
+let no = "3a"
+console.log(typeof no)
+
+
+
