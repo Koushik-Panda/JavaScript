6d46b65467f9ebeab297251fc81def41e9e2a4a6
Koushik Panda <koushik.3202@gmail.com>
2025-05-10 16:04:21 +0000

    Arrays

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,56 @@
+//array
+
+//const myarray = [ 0, 1, 2, 4, 5, "Koushik", true]
+const myaaray = [ 0, 1, 2, 4, 5]
+ // array - wriiten on third backet and inside of array all value called element
+//JavaScript Documention
+
+//JavaScript arrays are resizable and can contain a mix of different data types.
+//JavaScript arrays are not associative arrays and so, array elements cannot be accessed using arbitrary strings as indexes, but must be accessed using nonnegative integers (or their respective string form) as indexes.
+//JavaScript arrays are zero-indexed: the first element of an array is at index 0.
+//JavaScript array-copy operations create shallow copies. 
+
+// const myman = ["I", "am", "man"]
+
+
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr2[0]);
+// console.log(typeof myArr2)
+
+// array methods
+
+// myArr2.push(6)
+// myArr2.pop() // last value eliminted
+// myArr2.unshift(9)
+// myArr2.shift()
+// console.log(myArr2.includes(8))
+// console.log(myArr2.indexOf(2))
+
+//  const newArr = myArr2.join()
+
+//  console.log(myArr2)
+//  console.log(newArr)
+//  console.log(typeof newArr)
+
+ // slice, spice
+
+// console.log("A", myArr2)
+
+// const myArr2 = new Array(1,2,3,4)
+// const nyn2=myArr2.splice(1,3)
+// console.log("C",nyn2)
+// console.log(myArr2)
+//splice - splice() returns the removed elements from the array. it chnages the original but it does not folow indexing rule
+
+
+// const myArr2 = new Array(1,2,3,4)
+// const nyn1 = myArr2.slice(1,3)
+// console.log("B",nyn1)
+// console.log(myArr2)
+//slice - slice return selected element from the array. It does not change original it use a copy and follow indexing rule properly
+
+
+// console.log(myArr2);
+
+
+
